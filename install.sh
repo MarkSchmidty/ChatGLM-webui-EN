96610c4e0288cb543ad27de2b75033faee776ddc
Mark Schmidt <MarkSchmidty@users.noreply.github.com>
2023-03-22 00:43:07 -0400

    undo speedster

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
 pip install --upgrade -r requirements.txt
-pip install speedster
-python -m nebullvm.installers.auto_installer --compilers all
+#pip install speedster
+#python -m nebullvm.installers.auto_installer --compilers all
